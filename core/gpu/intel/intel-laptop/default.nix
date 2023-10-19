6803146ab30138510b9bd541bcd27e47dfba25e3
Brian F <130115651+wvpianoman@users.noreply.github.com>
2023-10-19 14:50:38 -0400

    update

--- core/gpu/intel/intel-laptop/default.nix
+++ core/gpu/intel/intel-laptop/default.nix
@@ -4,10 +4,7 @@
 
     # Configuration for  intel gpu acceleration & tlp
 
-    #./cpu-frequency
-    #./intel-acceleration.nix
-    #./tlp
-    ./HP-Folio-9470M/Eilite-Folio-9470M-HD-Intel-4000.nix
+    ./HD-INTEL.nix
     ./laptop-packages
 
   ];
