a216b62fe63f8f102c6ad159201407da51d649c0
wvpianoman <130115651+wvpianoman@users.noreply.github.com>
2023-10-18 21:55:52 -0400

    up

---
+++ user/default.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, stdenv, lib, ... }:
+
+#---------------------------------------------------------------------
+# Tolga Erok
+# 10/6/2023
+# My personal NIXOS KDE user configuration 
+# ¯\_(ツ)_/¯
+#---------------------------------------------------------------------
+
+{
+  imports = [
+
+    ../locale/america.nix
+    ./SOS/SOS.nix
+    ./brian/brian.nix
+    ./user-profile-pic
+
+  ];
+
+}
