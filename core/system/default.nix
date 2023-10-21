1c95160b88613fcc468c0dd92cff622e17c1c715
wvpianoman <130115651+wvpianoman@users.noreply.github.com>
2023-10-21 15:32:21 -0400

    update

--- core/system/default.nix
+++ core/system/default.nix
@@ -8,11 +8,6 @@
 
   imports = [
 
-    # ./blocker
-    # ./kernel-sysctl/SSD-28GB-default.nix       # kernel tweaks for laptop with SSD & 8GB RAM
-    # ./kernel-sysctl/SSD-default.nix               # kernel tweaks for laptop (General)
-    # ./theme    # GNOME THEME?
-    #./boot-kernel
     ./bluetooth
     ./documentation
     ./env
@@ -20,6 +15,7 @@
     ./firewall
     ./fonts
     ./network
+    ./zram
 
   ];
 
---
+++ core/system/zram/default.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  zramSwap = {  # Source: https://docs.kernel.org/admin-guide/blockdev/zram.html
+
+    enable = true;
+    #  algorithm = "lzo";  # Use LZO compression algorithm
+    #  algorithm = "zstd"; # Use Zstandard (zstd) compression algorithm
+    algorithm = "lz4";  # Use LZ4 compression algorithm
+    memoryPercent = 50;  #  % of ram used for compression
+
+    # Note: Load Module
+    # in terminal: sudo modprobe zram num_devices=4
+
+  };
+}
+
+
+# Zram Swap Configuration
+#zramSwap = {
+#  enable = true;     # Enable Zram Swap
+#  algorithm = "zstd";  # Use Zstandard (zstd) compression algorithm
+#  memoryPercent = 50; # Allocate 50% of RAM for Zram Swap
+#};
+
+# Zram Tmpfs Configuration
+#zramTmpfs = {
+#  enable = true;     # Enable Zram for tmpfs
+#  algorithm = "lz4";  # Use LZ4 compression algorithm
+##  memoryPercent = 25; # Allocate 25% of RAM for Zram tmpfs
+#};
+
+# Zram Extra Configuration
+#zramExtra = {
+#  enable = true;     # Enable Zram for an extra use case
+#  algorithm = "lzo";  # Use LZO compression algorithm
+#  memoryPercent = 30; # Allocate 30% of RAM for Zram extra use
+#};
+
+
+#You can copy and paste these configurations as needed.
+
+
+
+
+
