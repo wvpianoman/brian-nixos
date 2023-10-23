070878b2be3b76f852c2e01b3e15b1ed135901da
Brian Francisco <dbf.linux@gmail.com>
2023-10-23 19:48:36 -0400

    (ツ)_/¯    Update @ 07:48 PM

--- machines/LAPTOPS/Lenovo-Slim7/Lenovo-Slim7-configuration.nix
+++ machines/LAPTOPS/Lenovo-Slim7/Lenovo-Slim7-configuration.nix
@@ -130,6 +130,11 @@
   # -----------------------------------------------------------------
   services.xserver.libinput.enable = true;
 
+  # -----------------------------------------------------------------
+  #   Enable flatpak
+  # -----------------------------------------------------------------
+  services.flatpak.enable = true;
+
   environment.systemPackages = with pkgs;
     [
       #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
