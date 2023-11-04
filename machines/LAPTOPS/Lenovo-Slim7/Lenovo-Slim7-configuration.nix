9fc6e452897079689dcb8dbeb04045df15532709
Brian F <130115651+wvpianoman@users.noreply.github.com>
2023-11-03 22:24:07 -0400

    Update Lenovo-Slim7-configuration.nix

--- machines/LAPTOPS/Lenovo-Slim7/Lenovo-Slim7-configuration.nix
+++ machines/LAPTOPS/Lenovo-Slim7/Lenovo-Slim7-configuration.nix
@@ -103,7 +103,7 @@
   # -----------------------------------------------------------------
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-
+  services.xserver.displayManager.defaultSession = "plasmawayland";
   #---------------------------------------------------------------------
   # Enable automatic login for the user.
   #---------------------------------------------------------------------
