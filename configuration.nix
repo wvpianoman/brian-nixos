254ec6a3da50546867c764400ba1c9e9dda5cd6d
Brian F <130115651+wvpianoman@users.noreply.github.com>
2023-11-04 20:17:38 -0400

    (ツ)_/¯    Update @ 08:17 PM

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,24 @@
 # ¯\_(ツ)_/¯
 #---------------------------------------------------------------------
 
+#                                                                                               
+#                                                                                               
+#  ___      ___                  ____      ____                                                 
+#  `MM\     `M'68b              6MMMMb    6MMMMb\                                               
+#   MMM\     M Y89             8P    Y8  6M'    `                                               
+#   M\MM\    M ___ ____   ___ 6M      Mb MM               ____     ____         ____    _______ 
+#   M \MM\   M `MM `MM(   )P' MM      MM YM.             6MMMMb   6MMMMb       6MMMMb  ,MMMMMMM 
+#   M  \MM\  M  MM  `MM` ,P   MM      MM  YMMMMb        MM'  `Mb MM'  `Mb     6M'  `Mb dM       
+#   M   \MM\ M  MM   `MM,P    MM      MM      `Mb            ,MM       MM     MM    MM MP       
+#   M    \MM\M  MM    `MM.    MM      MM       MM           ,MM'      .M9     MM    MM M'       
+#   M     \MMM  MM    d`MM.   YM      M9       MM         ,M'      MMMM       MM    MM MMMMMMb  
+#   M      \MM  MM   d' `MM.   8b    d8  L    ,M9       ,M'           `Mb 68b YM.  ,M9      `Mb 
+#  _M_      \M _MM__d_  _)MM_   YMMMM9   MYMMMM9        MMMMMMMM       MM Y89  YMMMM9        MM 
+#                                                                      MM                    MM 
+#                                                                MM.  ,M9              MM.  ,M9 
+#                                                                 YMMMM9                YMMMM9  
+
+
 let
   
   #   Auto HOST chooser based on device product name
